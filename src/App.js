4112025721af7480cf87d85ce4c3b55bd561a7e0
rogiertenhacken <rogiertenhacken@gmail.com>
2018-06-12 11:24:45 +0200

    start of project

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        items: [],
+        isLoaded: false
+      }
+  }
+
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+
+  }
+
+  render() {
+
+    let { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+
+    else {
+      return (
+        <div className="App">
+
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                Name: {item.name} | Email: {item.email}
+              </li>
+            ))};
+          </ul>
+
+
+
+        </div>
+      );
+    }
+
+
+  }
+}
+
+export default App;
