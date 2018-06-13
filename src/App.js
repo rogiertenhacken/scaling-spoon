6ddc050bfb030c6f0da580a6a270921341358da1
rogiertenhacken <rogiertenhacken@gmail.com>
2018-06-13 11:53:51 +0200

    some api work

--- src/App.js
+++ src/App.js
@@ -6,23 +6,26 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
-        items: [],
+        MRData: [],
         isLoaded: false
       }
   }
 
+//https://jsonplaceholder.typicode.com/users
+//http://ergast.com/api/f1/2005/driverstandings/1.json
   componentDidMount() {
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
       .then(json => {
+        console.log(json);
         this.setState({
           isLoaded: true,
           items: json,
         })
+        console.log(this.state.items);
       });
-
-  }
+    }
 
   render() {
 
@@ -33,33 +36,65 @@ class App extends Component {
     }
 
     else {
+
       return (
         <div className="App">
-
           <table>
             <tbody>
               <tr>
                 {items.map(item => (
                   <td key={item.id}>
-                    Name: {item.name}
-
-                    <td key={item.id}>
-                      Email: {item.email}
-                    </td>
+                    Name: {item.name} | Email: {item.email}
                   </td>
-                ))};
+                ))}
               </tr>
             </tbody>
           </table>
-
-
-
         </div>
-      );
+      )}}
     }
 
-
-  }
-}
-
 export default App;
+
+// return (
+//   <div className="App">
+//
+//     <table>
+//       <tbody>
+//         <tr>
+//           {items.map(item => (
+//             <td key={item.id}>
+//               Name: {item.MRData.StandingsTable.season}
+//
+//               {/* <td key={item.id}>
+//                 Email: {item.email}
+//               </td> */}
+//             </td>
+//           ))};
+//         </tr>
+//       </tbody>
+//     </table>
+//   </div>
+// );
+
+///////
+
+/* <ul> give {}around this block
+  {items.map(item => (
+    <li key={item.id}>
+  Name: {item.name} | Email: {item.email}
+    </li>
+  ))}
+</ul> */
+
+// {items.map(item => (
+//   <td key={item.id}>
+//     Name: {item.driver} | Email: {item.season}
+//   </td>
+// ))}
+
+/* {items.map(item => (
+  <td key={item.id}>
+    Name: {item.name} | Email: {item.email}
+  </td>
+))} */
