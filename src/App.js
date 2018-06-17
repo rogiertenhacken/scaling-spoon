310cbc2d87406cb9411c5c2798ca24515674da25
rogiertenhacken <rogiertenhacken@gmail.com>
2018-06-17 13:36:27 +0200

    h1 image landingpage -rm mdl hover-border-table header-color var

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
 import './App.css'
-import { Layout, Content } from 'react-mdl'
 import Main from './components/main'
 
 class App extends Component {
   render () {
     return (
-      <div className='header-color' title='FORMULA 1'>
-        <h1>formula <span style={{fontSize: '.8em'}}>1</span></h1>
-        <Layout>
-          <Content>
-            <div className='page-content' />
-            <Main />
-          </Content>
-        </Layout>
+      <div className='header-color'>
+        <h1 className='title'>formula <span style={{fontSize: '.8em'}}>1</span></h1>
+        <div className='page-content' />
+        <Main />
       </div>
     )
   }
 }
 
 export default App
+
+// class App extends Component {
+//   render () {
+//     return (
+//       <div className='header-color' title='FORMULA 1'>
+//         <h1>formula <span style={{fontSize: '.8em'}}>1</span></h1>
+//         <Layout>
+//           <Content>
+//             <div className='page-content' />
+//             <Main />
+//           </Content>
+//         </Layout>
+//       </div>
+//     )
+//   }
+// }
